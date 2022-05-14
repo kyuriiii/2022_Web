3eb156b604bbfa78b799edf198606e412ee6383e
kyuriiii <47288015+kyuriiii@users.noreply.github.com>
2022-05-15 02:59:14 +0900

    feat: Assignment2 Set default ejs file

--- 과제2/routes/user.js
+++ 과제2/routes/user.js
@@ -3,21 +3,24 @@ const router = express.Router();
 const path = require('path');
 const fs = require('fs').promises;
 
-var users;
+var users = [];
 
-router.get('/', (req,res) => {
-    if ( req.session.user_ID != null ) res.redirect('/profile');
-    else res.redirect('/login');
+router.get('/', async (req,res) => {
+    console.log( "root - ", req.session.user_ID );
+    console.log( "root - ", req.signedCookies.is_login );
+    if ( req.signedCookies.is_login != null ) res.redirect('/profile');
+    else {
+        users = JSON.parse( await fs.readFile(path.join(__dirname,'../public/users.json')) );
+        res.render('login');
+    }
 });
 
 router.get('/login', async (req,res) => {
-    users = JSON.parse( await fs.readFile(path.join(__dirname,'../public/users.json')) );
-    res.render('login');
 });
 
 router.post('/login', (req,res) => {
     const { ID, pw } = req.body;
-    if( users[ID].pw != pw ) res.status(400).send({msg: process.env.LOGIN_FAIL});
+    if( users[ID] == null || users[ID].pw != pw ) res.status(400).send({msg: process.env.LOGIN_FAIL});
     else {
         res.cookie('is_login', true, {
             // expores: new Date(Date.now() + 3000),
@@ -30,7 +33,7 @@ router.post('/login', (req,res) => {
 
         req.session.user_ID= ID;
         req.session.save(function(){
-            res.redirect('profile');
+            res.redirect('/profile');
         });
     }
 });
@@ -42,7 +45,10 @@ router.get('/register', (req,res) => {
 router.post('/register', async (req,res) => {
     const { ID, pw, name, nickname, contact } = req.body;
 
-    if ( users[ID] != null ) res.redirect('login');
+    if ( users[ID] != null ) {
+        res.redirect('/');
+        return false;
+    }
     users[ID] = { pw, name, nickname, contact };
 
     await fs.writeFile(
@@ -50,28 +56,39 @@ router.post('/register', async (req,res) => {
         JSON.stringify(users)
     );
     
-    await res.redirect('login');
+    await res.redirect('/');
 });
 
 router.get('/profileAll', (req,res) => {
     res.render('profileAll', { users: users });
 });
 
+router.get('/profile', (req,res) => {
+        if ( !req.signedCookies.is_login ) {
+            res.redirect('/');
+            return false;
+        }
+
+        const ID = req.session.user_ID;
+        console.log( ID );
+        if ( users[ID] == null ) res.redirect('/logout');
+
+        res.render('profile', { ID: ID, user: users[ID] });
+    });
 router
-    .route('/profile')
-    .get((req,res) => {
-        if ( !req.cookies.is_login ) res.redirect('login');
-
-        const ID = req.session.user_ID;
-        if ( users[ID] != null ) res.redirect('login');
-
-        res.render('register', { user: users[ID] });
-    })
+    .route('/profile/:ID')
     .put( async (req,res) => {
-        if ( !req.cookies.is_login ) res.redirect('login');
+        if ( !req.signedCookies.is_login ) {
+            res.redirect('/');
+            return false;
+        }
         const ID = req.session.user_ID;
+        console.log( "ID : ", ID);
 
-        if ( ID != req.params.ID ) res.redirect('login');
+        if ( ID != req.params.ID ) {
+            res.redirect('/');
+            return false;
+        }
         
         const { pw, name, nickname, contact } = req.body;
         users[ID] = { pw, name, nickname, contact };
@@ -80,14 +97,19 @@ router
             path.join(__dirname, '../public/users.json'),
             JSON.stringify(users)
         );
-
-        res.send('success');
+        res.send("성공");
     })
     .delete( async (req,res) => {
-        if ( !req.cookies.is_login ) res.redirect('login');
+        if ( !req.signedCookies.is_login ) {
+            res.redirect('/');
+            return false;
+        }
         const ID = req.session.user_ID;
 
-        if ( ID != req.params.ID ) res.redirect('login');
+        if ( ID != req.params.ID ) {
+            res.redirect('/');
+            return false;
+        }
 
         delete users[ID];
 
@@ -95,7 +117,15 @@ router
             path.join(__dirname, '../public/users.json'),
             JSON.stringify(users)
         );
-        res.end();
+        
+        res.clearCookie('is_login', true, {
+            httpOnly: true,
+            path: '/',
+        });
+    
+        delete req.session.user_ID;
+        
+        res.send("성공");        
     });
 
 router.get('/logout', (req,res) => {
@@ -105,6 +135,8 @@ router.get('/logout', (req,res) => {
     });
 
     delete req.session.user_ID;
+
+    res.redirect('/');
 });
 
 module.exports = router;
